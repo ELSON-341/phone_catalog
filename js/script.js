5f3ee98078a69f07f126681d51c740a62c27d87d
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-05-01 13:59:16 -0300

    update in json

--- js/script.js
+++ js/script.js
@@ -3,15 +3,12 @@ fetch('products.json')
 .then(products => {
     const productsContainer = document.getElementById('products-container');
     console.log(products.price)
-    // products.forEach(product => {
-    //     console.log(product.price)
+        
+    products.forEach(element => {
+        const img = document.createElement('img')
+        
+        
+        console.log(img)
+    });
 
-    //     const productCard = document.createElement('div')
-    //     productCard.classList.add('product-card')
-
-    //     const figure = document.createElement('figure')
-    //     const img = document.createElement('img')
-    //     img.src = product.image
-    //     figure.appendChild(img)
-    // })
 })
